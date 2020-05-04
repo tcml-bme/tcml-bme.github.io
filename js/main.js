0809fa7658d2759827e721afc9a9a254de7aaca1
Moti Freiman <moti.freiman@bm.technion.ac.il>
2020-05-04 21:06:30 +0300

    main.js fix

--- js/main.js
+++ js/main.js
@@ -296,21 +296,16 @@ jQuery(document).ready(function($) {
 	siteSticky();
 
 	// navigation
-  var OnePageNavigation = function() {
-    var navToggler = $('.site-menu-toggle');
-   	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
-      e.preventDefault();
-
-      var hash = this.hash;
-
-      $('html, body').animate({
-        'scrollTop': $(hash).offset().top
-      }, 600, 'easeInOutCirc', function(){
-        window.location.hash = hash;
-      });
-
-    });
-  };
+	var OnePageNavigation = function() {
+		var navToggler = $('.site-menu-toggle');
+		$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu.site-nav-wrap li a", function(e) {
+			var hash = this.hash;
+				if(hash != ""){
+					e.preventDefault();
+					$('html, body').animate({'scrollTop': $(hash).offset().top}, 600, 'easeInOutCirc', function(){ window.location.hash = hash; });
+				}
+			});
+	};
   OnePageNavigation();
 
   var siteScroll = function() {
