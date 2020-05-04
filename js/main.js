20118451c1fa2786d7c70cba643984868ccab63d
Moti Freiman <moti.freiman@bm.technion.ac.il>
2020-05-04 21:13:37 +0300

    main.js fix

--- js/main.js
+++ js/main.js
@@ -298,7 +298,7 @@ jQuery(document).ready(function($) {
 	// navigation
 	var OnePageNavigation = function() {
 		var navToggler = $('.site-menu-toggle');
-		$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu.site-nav-wrap li a", function(e) {
+		$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
 			var hash = this.hash;
 				if(hash != ""){
 					e.preventDefault();
